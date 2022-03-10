f004ead0e709e26dfcd2112549f2b5135798f222
Vivek Kumar Singh <64417544+Vivek27951@users.noreply.github.com>
2022-03-10 10:02:19 +0530

    Update Todo.js

--- components/Todo.js
+++ components/Todo.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import { AiOutlinePlus, AiOutlineDelete } from "react-icons/Ai";
+import { AiOutlinePlus, AiOutlineDelete } from "react-icons/ai";
 
 const Todo = () => {
   const [item, setItem] = useState("");
